fd28c8936c711faa9cf68d3087b737b2f0a58071
MaxSheva <pianoruoff@gmail.com>
2017-01-30 04:25:08 +0200

    first commit

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TestController extends Controller
+{
+    /**
+     * test route
+     *
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function test()
+    {
+        return view('test');
+    }
+}
